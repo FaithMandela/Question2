75827d8ec496f63564423bb75f093b138dacbee0
Dennis Gichangi <dennis.gichangi@dewcis.com>
2015-07-31 15:14:19 +0300

    UEAB update

--- projects/ueab/database/08.changes.sql
+++ projects/ueab/database/08.changes.sql
@@ -1,89 +1,5 @@
 
 
-CREATE OR REPLACE FUNCTION getcummcredit(int) RETURNS float AS $$
-	SELECT sum(qgrades.credit)
-	FROM (qgrades INNER JOIN qstudents ON qgrades.qstudentid = qstudents.qstudentid)
-		INNER JOIN grades ON qgrades.gradeid = grades.gradeid
-	WHERE (qstudents.studentdegreeid = $1) AND (qstudents.approved = true) AND (qgrades.dropped = false)
-		AND (grades.gpacount = true) AND (qgrades.repeated = false) AND (qgrades.gradeid <> 'W') AND (qgrades.gradeid <> 'AW');
-$$ LANGUAGE SQL;
-
-CREATE OR REPLACE FUNCTION getcummgpa(int) RETURNS float AS $$
-	SELECT (CASE sum(qgrades.credit) WHEN 0 THEN 0 ELSE (sum(grades.gradeweight * qgrades.credit)/sum(qgrades.credit)) END)
-	FROM (qgrades INNER JOIN qstudents ON qgrades.qstudentid = qstudents.qstudentid)
-		INNER JOIN grades ON qgrades.gradeid = grades.gradeid
-	WHERE (qstudents.studentdegreeid = $1) AND (qstudents.approved = true)
-		AND (qgrades.dropped = false) AND (grades.gpacount = true) 
-		AND (qgrades.repeated = false) AND (qgrades.gradeid <> 'W') AND (qgrades.gradeid <> 'AW');
-$$ LANGUAGE SQL;
-
-DROP VIEW vw_studentdegrees;
-CREATE VIEW vw_studentdegrees AS
-	SELECT studentview.religionid, studentview.religionname, studentview.denominationid, studentview.denominationname,
-		studentview.schoolid, studentview.schoolname, studentview.studentid, studentview.studentname, studentview.address, studentview.zipcode,
-		studentview.town, studentview.addresscountry, studentview.telno, studentview.email,  studentview.guardianname, studentview.gaddress,
-		studentview.gzipcode, studentview.gtown, studentview.gaddresscountry, studentview.gtelno, studentview.gemail,
-		studentview.accountnumber, studentview.Nationality, studentview.Nationalitycountry, studentview.Sex,
-		studentview.MaritalStatus, studentview.birthdate, studentview.firstpass, studentview.alumnae, studentview.postcontacts,
-		studentview.onprobation, studentview.offcampus, studentview.currentcontact, studentview.currentemail, studentview.currenttel,
-		studentview.org_id,
-		sublevelview.degreelevelid, sublevelview.degreelevelname,
-		sublevelview.freshman, sublevelview.sophomore, sublevelview.junior, sublevelview.senior,
-		sublevelview.levellocationid, sublevelview.levellocationname,
-		sublevelview.sublevelid, sublevelview.sublevelname, sublevelview.specialcharges,
-		degrees.degreeid, degrees.degreename,
-		studentdegrees.studentdegreeid, studentdegrees.completed, studentdegrees.started, studentdegrees.cleared, studentdegrees.clearedate,
-		studentdegrees.graduated, studentdegrees.graduatedate, studentdegrees.dropout, studentdegrees.transferin, studentdegrees.transferout,
-		
-		studentdegrees.grad_apply, studentdegrees.grad_apply_date, studentdegrees.grad_finance, studentdegrees.grad_finance_date,
-		studentdegrees.grad_accept, studentdegrees.grad_accept_date,
-		
-		studentdegrees.mathplacement, studentdegrees.englishplacement, studentdegrees.details,
-		
-		getcummcredit(studentdegrees.studentdegreeid) as cumm_credits,
-		getcummgpa(studentdegrees.studentdegreeid) as cumm_gpa
-	FROM ((studentview INNER JOIN studentdegrees ON studentview.studentid = studentdegrees.studentid)
-		INNER JOIN sublevelview ON studentdegrees.sublevelid = sublevelview.sublevelid)
-		INNER JOIN degrees ON studentdegrees.degreeid = degrees.degreeid;
-
-CREATE OR REPLACE FUNCTION getoverload(real, float, float, float, boolean, float) RETURNS boolean AS $$
-DECLARE
-	myoverload boolean;
-BEGIN
-	myoverload := false;
-
-	IF ($1=14) THEN
-		IF (($3<1.99) AND ($2<>9)) THEN
-			myoverload := true;
-		ELSIF ($3 is null) AND ($2 > 14) THEN
-			myoverload := true;
-		ELSIF (($4>=110) AND ($3>=2.70) AND ($2<=17)) THEN
-			myoverload := false;
-		ELSE
-			IF (($3<3) AND ($2>14)) THEN
-				myoverload := true;
-			ELSIF (($3<3.5) AND ($2>15)) THEN
-				myoverload := true;
-			ELSIF ($2>16) THEN
-				myoverload := true;
-			END IF;
-		END IF;
-	ELSE
-		IF($2 > $1)THEN
-			myoverload := true;
-		END IF;
-	END IF;
-
-	IF (myoverload = true) THEN
-		IF ($5 = true) AND ($2 <= $6) THEN
-			myoverload := false;
-		END IF;
-	END IF;
-
-    RETURN myoverload;
-END;
-$$ LANGUAGE plpgsql;
-
 
 CREATE OR REPLACE FUNCTION grade_updates(varchar(12), varchar(12), varchar(12), varchar(12)) RETURNS varchar(240) AS $$
 BEGIN
@@ -119,10 +35,19 @@ BEGIN
 			AND (qgrades.dropped = false) AND (gradeid = 'IW')
 			AND (qstudents.quarterid = $1);
 	END IF;
+	
+	IF($3 = '5')THEN
+		UPDATE qgrades SET gradeid = 'AW', sys_audit_trail_id = $4::integer
+		FROM qstudents WHERE (qgrades.qstudentid = qstudents.qstudentid) 
+			AND (qgrades.dropped = false) AND (gradeid = 'UE')
+			AND (qstudents.quarterid = $1);
+	END IF;
 
 	RETURN 'Grade updates';
 END;
 $$ LANGUAGE plpgsql;
+
+
 
 
 CREATE OR REPLACE FUNCTION insQClose(varchar(12), varchar(12), varchar(12)) RETURNS VARCHAR(250) AS $$
@@ -227,24 +152,24 @@ BEGIN
 		IF(studentrec.probationdetail != null) THEN
 			mystr := '<br/>' || studentrec.probationdetail;
 		END IF;
-		RAISE EXCEPTION 'Student on Probation, See the Dean of Students % ', mystr;
+		RAISE EXCEPTION 'See your major adviser for courses you need to pick, get financial approval and the visit records office for approval % ', mystr;
 	ELSIF (studentrec.seeregistrar = true) THEN
 		IF(studentrec.probationdetail != null) THEN
 			mystr := '<br/>' || studentrec.probationdetail;
 		END IF;
-		RAISE EXCEPTION 'Cannot Proceed, Go to records office for clearance  % ', mystr;
+		RAISE EXCEPTION 'Go to records office for clearance  % ', mystr;
 	ELSIF (myrec.qstudentid IS NULL) THEN 
 		RAISE EXCEPTION 'Please register for the trimester, residence first before closing';
 	ELSIF (myrec.finalised = true) THEN
 		RAISE EXCEPTION 'The trimester is closed for registration';
     ELSIF (studentrec.gaddress IS NULL) THEN
-		RAISE EXCEPTION 'Cannot Proceed, See Records office, Wrong Guardian Address';
+		RAISE EXCEPTION 'Go to records office for clearance, Wrong Guardian Address';
 	ELSIF (studentrec.address IS NULL) THEN
-		RAISE EXCEPTION 'Cannot Proceed, See Records office, Wrong Student Address';
+		RAISE EXCEPTION 'Go to records office for clearance, Wrong Student Address';
 	ELSIF (myrec.finalised = true) THEN
 		RAISE EXCEPTION 'The trimester is closed for registration';
 	ELSIF (myprobation = true) THEN
-		RAISE EXCEPTION 'Your Cumm. GPA is below the required level, you need to see the registrar for apporval.';
+		RAISE EXCEPTION 'See your major adviser for courses you need to pick, get financial approval and the visit records office for approval';
 	ELSIF (v_last_reg = true) THEN
 		RAISE EXCEPTION 'You need to clear for late registration with the Registars office';
 	ELSIF (myqrec.qresidenceid is null) THEN
@@ -297,82 +222,4 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION getPGgradeid(integer) RETURNS varchar(2) AS $$
-	SELECT CASE WHEN max(gradeid) is null THEN 'NG' ELSE max(gradeid) END
-	FROM grades 
-	WHERE (p_minrange <= $1) AND (p_maxrange > $1);
-$$ LANGUAGE SQL;
 
-CREATE OR REPLACE FUNCTION updComputeGrade(varchar(12), varchar(12), varchar(12)) RETURNS varchar(240) AS $$
-DECLARE
-	v_qgradeid		integer;
-	msg				varchar(240);
-BEGIN
-	SELECT qgradeid INTO v_qgradeid
-	FROM qgrades
-	WHERE (qcourseid = CAST($1 as int)) AND ((lecture_marks + lecture_cat_mark) > 100);
-
-	IF(v_qgradeid is null)THEN
-		IF($3 = '1')THEN
-			UPDATE qgrades SET lecture_gradeid = getdbgradeid(round((lecture_marks + lecture_cat_mark)::double precision)::integer)
-			WHERE (qcourseid = CAST($1 as int));
-
-			msg := 'Lecturer Grade Computed Correctly';
-		END IF;
-		IF($3 = '2')THEN
-			UPDATE qgrades SET lecture_gradeid = getPGgradeid(round((lecture_marks + lecture_cat_mark)::double precision)::integer)
-			WHERE (qcourseid = CAST($1 as int));
-
-			msg := 'Lecturer Grade Computed Correctly';
-		END IF;
-	ELSE
-		msg := 'Some marks add up to more than 100';
-		RAISE EXCEPTION 'Some marks add up to more than 100';
-	END IF;
-	
-	RETURN msg;
-END;
-$$ LANGUAGE plpgsql;
-
-
-CREATE VIEW ws_students AS
-	SELECT students.studentid, students.schoolid, students.denominationid, students.org_id,
-		students.studentname, students.sex,
-		students.Nationality, students.MaritalStatus, students.birthdate, students.address,
-		students.zipcode, students.town, students.countrycodeid, 
-		students.telno,  students.email,
-		entitys.entity_id, entitys.entity_password,
-		studentdegrees.studentdegreeid, studentmajors.studentmajorid,
-		majors.majorid, majors.majorname, majors.departmentid
-	FROM students INNER JOIN entitys ON students.studentid = entitys.user_name
-		INNER JOIN studentdegrees ON students.studentid = studentdegrees.studentid
-		INNER JOIN studentmajors ON studentdegrees.studentdegreeid = studentmajors.studentdegreeid
-		INNER JOIN majors ON studentmajors.majorid = majors.majorid
-	WHERE (studentdegrees.completed = false) AND (studentmajors.primarymajor = true);
-
-CREATE VIEW ws_qstudents AS
-	SELECT ws_students.studentid, ws_students.schoolid, ws_students.denominationid, ws_students.org_id,
-		ws_students.studentname, ws_students.sex,
-		ws_students.Nationality, ws_students.MaritalStatus, ws_students.birthdate, ws_students.address,
-		ws_students.zipcode, ws_students.town, ws_students.countrycodeid, 
-		ws_students.telno,  ws_students.email,
-		ws_students.entity_id, ws_students.entity_password,
-		ws_students.studentdegreeid, ws_students.studentmajorid,
-		ws_students.majorid, ws_students.majorname, ws_students.departmentid,
-		qstudents.qstudentid, quarters.quarterid
-	FROM ws_students INNER JOIN qstudents ON ws_students.studentdegreeid = qstudents.studentdegreeid
-		INNER JOIN quarters ON qstudents.quarterid = quarters.quarterid
-	WHERE (quarters.active = true) AND (qstudents.approved = true);
-	
-	
-CREATE VIEW deans_list AS
-	SELECT quarterid, studentid, studentname, degreelevelid, degreelevelname, schoolname,
-		sublevelid, sublevelname, sex, 
-		studentdegreeid, qstudentid,
-		credit, gpa, cummcredit, cummgpa,
-		getcoremajor(studentdegreeid) as coremajor
-	FROM studentquartersummary
-	WHERE (credit >= 12) AND (gpa >= 3.5)
-		AND (checkincomplete(qstudentid) = 0) AND (checkgrade(qstudentid, 2.67) = 0)
-		AND (degreelevelid = 'UDG');		
-		
