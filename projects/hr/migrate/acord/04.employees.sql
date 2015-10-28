8e0544ef6a087956b4e9ea225c701f75c9c5e041
Dennis Gichangi <dennis.gichangi@dewcis.com>
2015-10-28 19:12:35 +0300

    ACORD HR Migration

---
+++ projects/hr/migrate/acord/04.employees.sql
@@ -0,0 +1,59 @@
+UPDATE employees SET surname = 'Ngige' ,first_name = 'Charles' , middle_name = ' Mungai', person_title  = 'Mr', place_of_birth = 'Nairobi', gender = 'M' WHERE entity_id = 56;
+UPDATE employees SET surname = 'Huynh' ,first_name = 'Chloe' , middle_name = 'Fang-Mei', person_title  = 'Ms', place_of_birth = 'Toulouse', gender = 'F' WHERE entity_id = 55;
+UPDATE employees SET surname = 'Maina' ,first_name = 'Gacheru' , middle_name = 'John', person_title  = 'Mr', place_of_birth = 'Nairobi',      gender = 'M' WHERE entity_id = 53;
+UPDATE employees SET surname = 'Muturia' ,first_name = 'Julian' , middle_name = 'Kagwiria', person_title  = 'Mrs', place_of_birth = 'MERU-KENYA', gender = 'F' WHERE entity_id = 52;
+UPDATE employees SET surname = 'Mutuku' ,first_name = 'Benson' , middle_name = 'Muthama', person_title  = 'Mr', place_of_birth = 'Machakos', gender = 'M' WHERE entity_id = 51;
+UPDATE employees SET surname = 'Zuriel' ,first_name = 'Salome' , middle_name = 'Mwende', person_title  = 'Mrs', place_of_birth = 'Nairobi', gender = 'F' WHERE entity_id = 47;
+UPDATE employees SET surname = 'Mulinda' ,first_name = 'Albanus' , middle_name = 'Munyoki', person_title  = 'Mr', place_of_birth = 'KITUI', gender = 'M' WHERE entity_id = 46;
+UPDATE employees SET surname = 'Ochieng' ,first_name = 'Anthony' , middle_name = '', person_title  = 'Mr', place_of_birth = 'Rachuonyo', gender = 'M' WHERE entity_id = 45;
+UPDATE employees SET surname = 'Fry' ,first_name = 'Thomas' , middle_name = 'Jake', person_title  = 'Mr', place_of_birth = 'London', gender = 'M' WHERE entity_id = 44;
+UPDATE employees SET surname = 'Ndiaye' ,first_name = 'Aminata' , middle_name = '', person_title  = 'Mrs', place_of_birth = 'SENEGAL', gender = 'F' WHERE entity_id = 41;
+UPDATE employees SET surname = 'Mbaye' ,first_name = 'Fatou' , middle_name = '', person_title  = 'Ms', place_of_birth = 'Ziguinchor', gender = 'F' WHERE entity_id = 37;
+UPDATE employees SET surname = 'Mathenge' ,first_name = 'Michelle' , middle_name = 'Nyawira', person_title  = 'Mrs', place_of_birth = 'Nairobi', gender = 'F' WHERE entity_id = 38;
+UPDATE employees SET surname = 'Odour' ,first_name = 'Sellesor' , middle_name = '', person_title  = 'Ms', place_of_birth = 'Kisumu', gender = 'F' WHERE entity_id = 34;
+UPDATE employees SET surname = 'Ngum' ,first_name = 'Ousainou' , middle_name = '', person_title  = 'Mr', place_of_birth = 'Banjul', gender = 'M' WHERE entity_id = 33;
+UPDATE employees SET surname = 'Bajenja' ,first_name = 'Ellen' , middle_name = 'Kajura', person_title  = 'Mrs', place_of_birth = 'Fort', gender = 'F' WHERE entity_id = 31;
+UPDATE employees SET surname = 'Wamukoya' ,first_name = 'Margaret' , middle_name = '', person_title  = 'Mrs', place_of_birth = 'Busia', gender = 'F' WHERE entity_id = 28;
+UPDATE employees SET surname = 'Msabeni-Ngoye' ,first_name = 'Annette' , middle_name = 'Rwezenda', person_title  = 'Mrs', place_of_birth = 'Nairobi', gender = 'F' WHERE entity_id = 27;
+UPDATE employees SET surname = 'Sendegeya' ,first_name = 'Leonie' , middle_name = 'Abela', person_title  = 'Mrs', place_of_birth = 'KAMPALA/UGANDA', gender = 'F' WHERE entity_id = 26;
+UPDATE employees SET surname = 'Oluoch' ,first_name = 'Wilkister' , middle_name = 'Akinyi', person_title  = 'Mrs', place_of_birth = 'NAIROBI', gender = 'F' WHERE entity_id = 27;
+UPDATE employees SET surname = 'Lutwama' ,first_name = 'Elijah' , middle_name = '', person_title  = 'Mr', place_of_birth = 'Kampala', gender = 'M' WHERE entity_id = 22;
+UPDATE employees SET surname = 'Otieno' ,first_name = 'Stephen' , middle_name = 'Odiwuor', person_title  = 'Mr', place_of_birth = 'Kisumu', gender = 'M' WHERE entity_id = 21;
+UPDATE employees SET surname = 'Aduol' ,first_name = 'Rose' , middle_name = '', person_title  = 'Mrs', place_of_birth = 'KENYA', gender = 'F' WHERE entity_id = 20;
+UPDATE employees SET surname = 'Havyarimana' ,first_name = 'Sophie' , middle_name = '', person_title  = 'Mrs', place_of_birth = 'Higiro', gender = 'F' WHERE entity_id = 19;
+UPDATE employees SET surname = 'Sanou' ,first_name = 'Salina' , middle_name = 'Cheserem', person_title  = 'Mrs', place_of_birth = 'Marakwet', gender = 'F' WHERE entity_id = 17;
+UPDATE employees SET surname = 'Gikonyo-Mukoko' ,first_name = 'Helen' , middle_name = 'Wangechi', person_title  = 'Mrs', place_of_birth = 'Nyeri', gender = 'F' WHERE entity_id = 12; 
+UPDATE employees SET surname = 'Bevan' ,first_name = 'Nicola' , middle_name = '', person_title  = 'Ms', place_of_birth = 'Dublin', gender = 'F' WHERE entity_id = 11;
+UPDATE employees SET surname = 'Wafula' ,first_name = 'James' , middle_name = 'Were', person_title  = 'Mr', place_of_birth = '', gender = 'M' WHERE entity_id = 35;
+UPDATE employees SET surname = 'Dupont' ,first_name = 'Vanessa' , middle_name = '', person_title  = 'Mrs', place_of_birth = 'France', gender = 'F' WHERE entity_id = 9;
+UPDATE employees SET surname = 'Seljeflot' ,first_name = 'Kristin' , middle_name = '', person_title  = 'Mrs', place_of_birth = 'Arendal', gender = 'F' WHERE entity_id = 8;
+UPDATE employees SET surname = 'Foday-Khabenje' ,first_name = 'Almonda' , middle_name = 'Patricia', person_title  = 'Mrs', place_of_birth = 'Freetown', gender = 'F' WHERE entity_id = 2;
+
+DELETE FROM employees WHERE entity_id NOT in(56,55, 53,52,51,47,46,45,44,41,37,38,34,33,31,28,27,26,23,22,21,20,19,17,12,11,35,9,8,2,0,1);
+DELETE FROM entity_subscriptions WHERE entity_id NOT IN(56,55, 53,52,51,47,46,45,44,41,37,38,34,33,31,28,27,26,23,22,21,20,19,17,12,11,35,9,8,2,0,1);
+DELETE FROM default_tax_types WHERE entity_id NOT IN(56,55, 53,52,51,47,46,45,44,41,37,38,34,33,31,28,27,26,23,22,21,20,19,17,12,11,35,9,8,2,0,1);
+DELETE FROM education WHERE entity_id NOT IN(56,55, 53,52,51,47,46,45,44,41,37,38,34,33,31,28,27,26,23,22,21,20,19,17,12,11,35,9,8,2,0,1);
+DELETE FROM identifications WHERE entity_id NOT IN(56,55, 53,52,51,47,46,45,44,41,37,38,34,33,31,28,27,26,23,22,21,20,19,17,12,11,35,9,8,2,0,1);
+DELETE FROM kins WHERE entity_id NOT IN(56,55, 53,52,51,47,46,45,44,41,37,38,34,33,31,28,27,26,23,22,21,20,19,17,12,11,35,9,8,2,0,1);
+DELETE FROM entitys WHERE entity_id NOT IN(56,55, 53,52,51,47,46,45,44,41,37,38,34,33,31,28,27,26,23,22,21,20,19,17,12,11,35,9,8,2,0,1);
+
+UPDATE entitys SET entity_name = (employees.Surname || ' ' || employees.First_name || ' ' || COALESCE(employees.Middle_name, '')) 
+FROM employees
+WHERE employees.entity_id = entitys.entity_id;
+
+INSERT INTO employee_leave_types (entity_id, org_id, leave_type_id, leave_starting)
+SELECT employees.entity_id,employees.org_id, leave_types.leave_type_id, '2015-01-01'::date
+FROM leave_types CROSS JOIN employees
+WHERE (leave_types.use_type = 1);
+
+INSERT INTO employee_leave_types (entity_id, org_id, leave_type_id, leave_starting)
+SELECT employees.entity_id,employees.org_id, leave_types.leave_type_id, '2015-01-01'::date
+FROM leave_types CROSS JOIN employees
+WHERE (leave_types.use_type = 2) AND (employees.gender = 'F');
+
+INSERT INTO employee_leave_types (entity_id, org_id, leave_type_id, leave_starting)
+SELECT employees.entity_id,employees.org_id, leave_types.leave_type_id, '2015-01-01'::date
+FROM leave_types CROSS JOIN employees
+WHERE (leave_types.use_type = 3) AND (employees.gender = 'M');
+
+
