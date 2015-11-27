33dda5c23653a36d1d9bb2ee8e207eec12eb353b
Dennis Gichangi <dennis.gichangi@dewcis.com>
2015-11-27 08:02:00 +0300

    ACORD Project

--- projects/acord/database/02.view.sql
+++ projects/acord/database/02.view.sql
@@ -1,111 +1,111 @@
 
-CREATE VIEW programmeview AS
-	SELECT DISTINCT programmes.programmeid, programmes.themeid, programmes.sys_country_id, 
-		programmes.programmename, programmes.location, programmes.address, programmes.town, programmes.email,
-		programmes.telephone, programmes.fax, programmes.starteddate, programmes.duration, programmes.income, 
-		programmes.budget, programmes.femployees, programmes.memployees, programmes.programmebrief, 
-		programmes.history, programmes.objectives, 
-		programmes.thematicprogramme, programmes.areaprogramme, programmes.panafricanprogramme, programmes.details,
-		sys_countrys.sys_country_name
-	FROM ((programmes INNER JOIN sys_countrys ON programmes.sys_country_id=sys_countrys.sys_country_id) INNER JOIN
-		themes ON programmes.themeid=themes.themeid) 
-	ORDER BY sys_country_name;
+CREATE VIEW vw_donors AS
+	SELECT donor_groups.donor_group_id, donor_groups.donor_group_name, 
+		sys_countrys.sys_country_id, sys_countrys.sys_country_name, 
+		donors.org_id, donors.donor_id, donors.donor_name, donors.legal_status, 
+		donors.address, donors.telephone, donors.fax, donors.email, donors.skype, 
+		donors.contact_person, donors.contact_person_title, donors.physical_address, 
+		donors.website, donors.social_media_links, donors.details
+	FROM donors INNER JOIN donor_groups ON donors.donor_group_id = donor_groups.donor_group_id
+		INNER JOIN sys_countrys ON donors.nationality_id = sys_countrys.sys_country_id;
 
-CREATE VIEW programmecountryview AS
-	SELECT DISTINCT programmes.programmename, programmes.programmeid,
-			sys_countrys.sys_country_name, sys_countrys.sys_country_id 
-	FROM programmes INNER JOIN sys_countrys ON programmes.sys_country_id=sys_countrys.sys_country_id;
-
-CREATE VIEW donorview AS
-	SELECT sys_countrys.sys_country_id, sys_countrys.sys_country_name, donorgroups.donorgroupid,
-		donorgroups.donorgroupname, donors.donorid, donors.donorname, donors.headquarters, donors.contactperson, 
-		donors.address, donors.town, donors.email, donors.telephone, donors.fax, donors.localdonor, 
-		donors.memberdonor, donors.fundingsummary, donors.donorsummary, donors.details
-	FROM ((sys_countrys INNER JOIN donors ON sys_countrys.sys_country_id = donors.sys_country_id)
-		INNER JOIN donorgroups ON donors.donorgroupid = donorgroups.donorgroupid);
-
-CREATE VIEW budgetview AS
-	SELECT budget.budgetid, budget.expenditure, budget.budgettype, budget.globalamount, 
-		budget.fieldamount, budget.getbydate, budget.spendbydate, budget.officeid,
-		projects.projectid, projects.projectname, subthemes.subthemename
-	FROM ((budget INNER JOIN projects ON budget.projectid=projects.projectid)	 
-		INNER JOIN subthemes ON budget.subthemeid=subthemes.subthemeid);
-
-CREATE VIEW contractview AS
-	SELECT contracts.contractid, contracts.applicationref, contracts.currency, contracts.corefund, 
-		contracts.percentagelevy, contracts.contractref, contracts.decisiondate, contracts.contractdate,
-		contracts.startofgrant, contracts.endofgrant, contracts.conditions, contracts.notes, contracts.subject,
-		contracts.financing, contracts.reporting, contracts.operation, contracts.genconditions, 
-		contracts.speconditions, contracts.details, projects.projectid, projects.projectname, donors.donorname
-	FROM ((contracts INNER JOIN projects ON contracts.projectid=projects.projectid) INNER JOIN donors ON 
-		contracts.donorid=donors.donorid);
-
-CREATE VIEW projectview AS
-	SELECT  projects.projectid, projects.projectname, projects.location, projects.sys_country_id,
-		projects.regionaldesk, projects.address, projects.town, projects.email, projects.telephone, projects.fax,
-		projects.startingdate, projects.openingbalance, projects.history, projects.core,
-		projects.objectives, projects.details, programmes.programmeid,
-		programmes.programmename, programmes.income, programmes.budget, sys_countrys.sys_country_name
-	FROM ((projects INNER JOIN programmes ON projects.programmeid= programmes.programmeid) 
-		INNER JOIN sys_countrys ON projects.sys_country_id=sys_countrys.sys_country_id);
-
-CREATE VIEW grantview AS
-	SELECT grants.grantid, grants.contractid, grants.amount, grants.prdate, grants.details, contracts.contractref,
-		projects.projectname	
-	FROM ((grants INNER JOIN contracts ON grants.contractid=contracts.contractid) INNER JOIN projects ON
-		projects.projectid=contracts.projectid);
-
-CREATE VIEW bankingview AS
-	SELECT banking.bankid, banking.contractid, banking.exchangecodeid, banking.referenceno, banking.receivedate,
-		banking.amount, banking.exchangerate, banking.details, projects.projectname, programmes.programmename
-	FROM (((banking INNER JOIN contracts ON banking.contractid=contracts.contractid) INNER JOIN projects ON 
-		contracts.projectid=projects.projectid) INNER JOIN programmes ON 
-		projects.programmeid=programmes.programmeid);
-
-CREATE VIEW proposalview AS
-	SELECT proposals.proposalid, proposals.programmeid, proposals.themeid, proposals.startdate, proposals.description,
-		proposals.location, proposals.budget, proposals.proposal, proposals.details,
-		programmes.programmename,themes.themename
-	FROM ((proposals INNER JOIN programmes ON proposals.programmeid=programmes.programmeid) INNER JOIN themes ON
-		proposals.themeid=themes.themeid);
-
-CREATE VIEW submissionview AS
-	SELECT submissions.submissionid, submissions.proposalid, submissions.proposalstatusid,	submissions.donorid,
-		submissions.submitdatedate, submissions.email, submissions.approved, submissions.dropped, 
-		submissions.details, proposals.description, proposalstatus.proposalstatuname, donors.donorname
-	FROM (((submissions INNER JOIN proposals ON submissions.proposalid=proposals.proposalid) INNER JOIN proposalstatus
-		ON submissions.proposalstatusid=proposalstatus.proposalstatusid) INNER JOIN donors ON
-		submissions.donorid=donors.donorid);
-
-CREATE VIEW activityview AS
-	SELECT activities.activityid, activities.projectid, activities.activity, activities.startdate, activities.closedate,
-		activities.details, activities.impactrating, activities.subthemeid, projects.projectname, subthemes.subthemename
-	FROM ((activities INNER JOIN projects ON activities.projectid=projects.projectid) INNER JOIN subthemes
-		 ON activities.subthemeid=subthemes.subthemeid);
-
-CREATE VIEW expenditureview AS
-	SELECT expenditure.expenditureid, expenditure.projectid, expenditure.officeid, 
-		expenditure.amount, expenditure.prdate, expenditure.details, projects.projectname, 
-		subthemes.subthemename, programmes.programmename
-	FROM (((expenditure INNER JOIN subthemes ON expenditure.subthemeid=subthemes.subthemeid) INNER JOIN
-		projects ON expenditure.projectid=projects.projectid) INNER JOIN programmes ON 
-		projects.programmeid=programmes.programmeid);
-
-CREATE VIEW deadlineview AS
-	SELECT deadlines.deadlineid, deadlines.contractid, deadlines.event, deadlines.eventdate, deadlines.email, 
-		deadlines.details, contracts.contractref, projects.projectname, projects.projectid
-	FROM ((deadlines INNER JOIN contracts ON deadlines.contractid=contracts.contractid) INNER JOIN projects
-		ON contracts.projectid=projects.projectid);
-
-CREATE VIEW officeview AS
-	SELECT offices.officeid, offices.officename, offices.sys_country_id, offices.details,
-		sys_countrys.sys_country_name
-	FROM offices INNER JOIN sys_countrys ON offices.sys_country_id=sys_countrys.sys_country_id;
-
-CREATE VIEW themeview AS
-	SELECT subthemes.subthemeid, subthemes.subthemename, subthemes.details,
-		themes.themeid, themes.themename	
-	FROM subthemes INNER JOIN themes ON subthemes.themeid=themes.themeid;	
+CREATE VIEW vw_targets AS
+	SELECT goals.goal_id, goals.goal_name, 
+		targets.org_id, targets.target_id, targets.target_name, targets.details
+	FROM targets INNER JOIN goals ON targets.goal_id = goals.goal_id;
 	
+CREATE VIEW vw_project_locations AS
+	SELECT projects.project_id, projects.project_title, 
+		sys_countrys.sys_country_id, sys_countrys.sys_country_name, 
+		project_locations.org_id, project_locations.project_location_id, project_locations.region_covered, 
+		project_locations.details
+	FROM project_locations INNER JOIN projects ON project_locations.project_id = projects.project_id
+		INNER JOIN sys_countrys ON project_locations.sys_country_id = sys_countrys.sys_country_id;
+		
+CREATE VIEW vw_project_goals AS
+	SELECT goals.goal_id, goals.goal_name, 
+		projects.project_id, projects.project_title, 
+		project_goals.org_id, project_goals.project_goal_id, project_goals.goal_ps, project_goals.details
+	FROM project_goals INNER JOIN goals ON project_goals.goal_id = goals.goal_id
+		INNER JOIN projects ON project_goals.project_id = projects.project_id;
 
+CREATE VIEW vw_goal_targets AS
+	SELECT vw_targets.goal_id, vw_targets.goal_name, 
+		vw_targets.target_id, vw_targets.target_name, 
+		project_goals.org_id, project_goals.project_goal_id, project_goals.goal_ps,
+		project_goals.project_id
+	FROM project_goals INNER JOIN vw_targets ON project_goals.goal_id = vw_targets.goal_id;
+		
+CREATE VIEW vw_project_targets AS
+	SELECT vw_project_goals.goal_id, vw_project_goals.goal_name, 
+		vw_project_goals.project_id, vw_project_goals.project_title,
+		vw_project_goals.project_goal_id, vw_project_goals.goal_ps,
+		targets.target_id, targets.target_name, 
+		
+		project_targets.org_id, project_targets.project_targets_id, project_targets.project_targets_name, 
+		project_targets.detscription
+	FROM project_targets INNER JOIN vw_project_goals ON project_targets.project_goal_id = vw_project_goals.project_goal_id
+		INNER JOIN targets ON project_targets.target_id = targets.target_id;
+		
+CREATE VIEW vw_contracts AS
+	SELECT projects.project_id, projects.project_title,
+		donors.donor_id, donors.donor_name,
+		currency.currency_id, currency.currency_name, 
+		contracts.org_id, contracts.contract_id, contracts.application_ref, contracts.core_fund, 
+		contracts.percentage_levy, contracts.contract_ref, contracts.decision_date, contracts.contract_date, 
+		contracts.start_of_grant, contracts.end_of_grant, contracts.conditions, contracts.notes, contracts.subject, 
+		contracts.financing, contracts.reporting, contracts.operation, contracts.general_conditions, 
+		contracts.special_conditions, contracts.details
+	FROM contracts INNER JOIN projects ON contracts.project_id = projects.project_id
+		INNER JOIN donors ON contracts.donor_id = donors.donor_id
+		INNER JOIN currency ON contracts.currency_id = currency.currency_id;
+		
+CREATE VIEW vw_proposals AS
+	SELECT projects.project_id, projects.project_title,
+		donors.donor_id, donors.donor_name,
+		proposal_status.proposal_status_id, proposal_status.proposal_status_name, 
+		proposals.org_id, proposals.proposal_id, proposals.start_date, proposals.description, 
+		proposals.location, proposals.proposal_submit_date, proposals.email, proposals.approved, 
+		proposals.dropped, proposals.budget, proposals.proposal, proposals.details
+	FROM proposals INNER JOIN projects ON proposals.project_id = projects.project_id
+		INNER JOIN donors ON proposals.donor_id = donors.donor_id
+		INNER JOIN proposal_status ON proposals.proposal_status_id = proposal_status.proposal_status_id;
 
+CREATE VIEW vw_activities AS
+	SELECT projects.project_id, projects.project_title, 
+		activities.org_id, activities.activity_id, activities.activity, activities.activity_start_date, 
+		activities.activity_close_date, activities.details
+	FROM activities INNER JOIN projects ON activities.project_id = projects.project_id;
+	
+CREATE VIEW vw_grants AS
+	SELECT vw_contracts.project_id, vw_contracts.project_title,
+		vw_contracts.donor_id, vw_contracts.donor_name,
+		vw_contracts.currency_id, vw_contracts.currency_name, 
+		vw_contracts.contract_id, vw_contracts.application_ref, vw_contracts.core_fund, 
+		vw_contracts.percentage_levy, vw_contracts.contract_ref, vw_contracts.decision_date, vw_contracts.contract_date, 
+		vw_contracts.start_of_grant, vw_contracts.end_of_grant, vw_contracts.conditions, vw_contracts.notes, vw_contracts.subject, 
+		vw_contracts.financing, vw_contracts.reporting, vw_contracts.operation, vw_contracts.general_conditions, 
+		vw_contracts.special_conditions, 
+		grants.org_id, grants.grant_id, grants.grant_amount, grants.grant_pr_date, grants.details
+	FROM grants INNER JOIN vw_contracts ON grants.contract_id = vw_contracts.contract_id;
+
+CREATE VIEW vw_budget AS
+	SELECT projects.project_id, projects.project_title,
+		budget_type.budget_type_id, budget_type.budget_type_name, 
+		currency.currency_id, currency.currency_name, 
+		budget.org_id, budget.budget_id, budget.global_amount, budget.field_amount, budget.exchange_rate,
+		budget.get_by_date, budget.spend_by_date, budget.details,
+		(budget.global_amount * budget.exchange_rate) as base_global_amount,
+		(budget.field_amount * budget.exchange_rate) as base_field_amount
+	FROM budget INNER JOIN projects ON budget.project_id = projects.project_id
+		INNER JOIN budget_type ON budget.budget_type_id = budget_type.budget_type_id
+		INNER JOIN currency ON budget.currency_id = currency.currency_id;
+		
+CREATE VIEW vw_expenditure AS
+	SELECT projects.project_id, projects.project_title,
+		currency.currency_id, currency.currency_name, 
+		expenditure.org_id, expenditure.expenditure_id, 
+		expenditure.amount, expenditure.exchange_rate, expenditure.pr_date, expenditure.details,
+		(expenditure.amount * expenditure.exchange_rate) as base_amount
+	FROM expenditure INNER JOIN projects ON expenditure.project_id = projects.project_id
+		INNER JOIN currency ON expenditure.currency_id = currency.currency_id;
