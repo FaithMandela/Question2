65f72e39b9b0ddac1e68b5bfc6aed016c61b95b8
Dennis Gichangi <dennis.gichangi@dewcis.com>
2016-01-20 07:51:08 +0300

    License update

---
+++ src/org/baraza/com/BLicense.java
@@ -0,0 +1,20 @@
+/**
+ * @author      Dennis W. Gichangi <dennis@openbaraza.org>
+ * @version     2011.0329
+ * @since       1.6
+ * website		www.openbaraza.org
+ * The contents of this file are subject to the GNU Lesser General Public License
+ * Version 3.0 ; you may use this file in compliance with the License.
+ */
+package org.baraza.com;
+
+public class BLicense {
+
+	public BLicense() {
+	
+	}
+	
+	
+
+
+}
