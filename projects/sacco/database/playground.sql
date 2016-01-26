8963e13d20dfdb11c9745862c63d76c85abd9be7
Ignatius Mang'eere <ignatius.mangeere@dewcis.com>
2016-01-26 17:08:12 +0300

    Sacco update with new xml, loan module updated and changed

--- projects/sacco/database/playground.sql
+++ projects/sacco/database/playground.sql
@@ -1,18 +1,15 @@
-
-CREATE VIEW vw_loans AS
-	SELECT 	vw_loans.currency_id, vw_loans.currency_name,vw_loans.currency_symbol,
-		vw_loans.loan_type_id, vw_loans.loan_type_name, 
-		vw_loans.entity_id, vw_loans.entity_name,
-		vw_loans.org_id, vw_loans.loan_id, vw_loans.principle, vw_loans.interest, vw_loans.monthly_repayment, vw_loans.reducing_balance, 
-		vw_loans.repayment_period,vw_loans.application_date, vw_loans.approve_status, vw_loans.initial_payment, 
-		vw_loans.loan_date, vw_loans.action_date,vw_loans.details,
-		vw_loans.repayment_amount, vw_loans.total_interest, vw_loans. loan_balance,
-		loan_repayment.loan_repayment_id, loan_repayment.period_id,
-		loan_repayment.repayment_amount as loan_repayment_amount,
-		loan_repayment.repayment_interest,
-		loan_repayment.penalty, loan_repayment.penalty_paid,
-		loan_repayment.repayment_narrative,
-		vw_loans.calc_repayment_period
+CREATE VIEW vw_gurrantors AS  
+	SELECT vw_loans.principle,vw_loans.entity_id, vw_loans.interest,
+		vw_loans.monthly_repayment,vw_loans.loan_date,vw_loans.initial_payment,	vw_loans.loan_id,vw_loan.repayment_amount,vw_loans.total_interest,
+		vw_loans.loan_balance,vw_loans.calc_repayment_period,vw_loans.reducing_balance, vw_loans.repayment_period,vw_loans.application_date,vw_loans.approve_status,vw_loans.org_id,
+		,vw_loans.action_date,vw_loans.details,
+	
+		entitys.entity_name,entitys.is_picked,
+		loan_types.loan_type_id,loan_types.loan_type_name,loan_types.default_interest,gurrantors.gurrantor_id,
+		gurrantors.is_accepted,gurrantors.amount,gurrantors_entity.entity_name as gurrantor_entity_name,
+		gurrantors_entity.entity_id AS gurrantor_entity_id
 	FROM vw_loans
-	INNER JOIN loan_repayment ON loan_repayment.loan_id = vw_loans.loan_id
-
+		JOIN entitys ON loans.entity_id = entitys.entity_id
+		JOIN loan_types ON loans.loan_type_id = loan_types.loan_type_id
+		JOIN gurrantors ON gurrantors.loan_id = loans.loan_id
+		JOIN entitys gurrantors_entity ON entitys.entity_id = gurrantors.entity_id;
