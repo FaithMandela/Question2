1432d9e3d01eb72a1cd8e475c288ee811ed801d7
Dennis Gichangi <dennis.gichangi@dewcis.com>
2016-02-03 20:12:57 +0300

    HR Update

--- projects/hr/database/22.update.sql
+++ projects/hr/database/22.update.sql
@@ -1,66 +1,66 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-
-CREATE OR REPLACE FUNCTION ins_subscriptions() RETURNS trigger AS $$
+CREATE OR REPLACE FUNCTION ins_loans() RETURNS trigger AS $$
 DECLARE
-	v_entity_id		integer;
-	v_org_id		integer;
-	v_currency_id	integer;
-	v_department_id	integer;
-	v_bank_id		integer;
-	v_org_suffix    char(2);
-	rec 			RECORD;
+	v_default_interest	real;
+	v_reducing_balance	boolean;
 BEGIN
 
-	IF (TG_OP = 'INSERT') THEN
-		SELECT entity_id INTO v_entity_id
-		FROM entitys WHERE lower(trim(user_name)) = lower(trim(NEW.primary_email));
-
-		IF(v_entity_id is null)THEN
-			NEW.entity_id := nextval('entitys_entity_id_seq');
-			INSERT INTO entitys (entity_id, org_id, entity_type_id, entity_name, User_name, primary_email,  function_role, first_password)
-			VALUES (NEW.entity_id, 0, 5, NEW.primary_contact, lower(trim(NEW.primary_email)), lower(trim(NEW.primary_email)), 'subscription', null);
-		
-			INSERT INTO sys_emailed (sys_email_id, org_id, table_id, table_name)
-			VALUES (4, 0, NEW.entity_id, 'subscription');
-		
-			NEW.approve_status := 'Completed';
-			NEW.system_key := uuid_generate_v4();
-		ELSE
-			RAISE EXCEPTION 'You already have an account, login and request for services';
-		END IF;
-	ELSIF(NEW.approve_status = 'Approved')THEN
-
-		NEW.org_id := nextval('orgs_org_id_seq');
-		INSERT INTO orgs(org_id, currency_id, org_name, org_sufix, default_country_id)
-		VALUES(NEW.org_id, 2, NEW.business_name, NEW.org_id, NEW.country_id);
-		
-		v_currency_id := nextval('currency_currency_id_seq');
-		INSERT INTO currency (org_id, currency_id, currency_name, currency_symbol) VALUES (NEW.org_id, v_currency_id, 'US Dollar', 'USD');
-		v_currency_id := nextval('currency_currency_id_seq');
-		INSERT INTO currency (org_id, currency_id, currency_name, currency_symbol) VALUES (NEW.org_id, v_currency_id, 'Euro', 'ERO');
-		UPDATE orgs SET currency_id = v_currency_id WHERE org_id = NEW.org_id;
-		
-		INSERT INTO pay_scales (org_id, pay_scale_name, min_pay, max_pay) VALUES (NEW.org_id, 'Basic', 0, 1000000);
-		INSERT INTO pay_groups (org_id, pay_group_name) VALUES (NEW.org_id, 'Default');
-		INSERT INTO locations (org_id, location_name) VALUES (NEW.org_id, 'Main office');
-
-		v_department_id := nextval('departments_department_id_seq');
-		INSERT INTO Departments (org_id, department_id, department_name) VALUES (NEW.org_id, v_department_id, 'Board of Directors');
-		INSERT INTO department_roles (org_id, department_id, department_role_name, active) VALUES (NEW.org_id, v_department_id, 'Board of Directors', true);
-		
-		v_bank_id := nextval('banks_bank_id_seq');
-		INSERT INTO banks (org_id, bank_id, bank_name) VALUES (NEW.org_id, v_bank_id, 'Cash');
-		INSERT INTO bank_branch (org_id, bank_id, bank_branch_name) VALUES (NEW.org_id, v_bank_id, 'Cash');
-		
-		UPDATE entitys SET org_id = NEW.org_id, function_role='subscription,admin,staff,finance'
-		WHERE entity_id = NEW.entity_id;
-
-		INSERT INTO sys_emailed (sys_email_id, org_id, table_id, table_name)
-		VALUES (5, NEW.org_id, NEW.entity_id, 'subscription');
+	SELECT default_interest, reducing_balance INTO v_default_interest, v_reducing_balance
+	FROM loan_types 
+	WHERE (loan_type_id = NEW.loan_type_id);
+	
+	IF(NEW.interest is null)THEN
+		NEW.interest := v_default_interest;
 	END IF;
+	IF (NEW.reducing_balance is null)THEN
+		NEW.reducing_balance := v_reducing_balance;
+	END IF;
+	IF(NEW.monthly_repayment is null) THEN
+		NEW.monthly_repayment := 0;
+	END IF;
+	IF (NEW.repayment_period is null)THEN
+		NEW.repayment_period := 0;
+	END IF;
+	
 
+	IF(NEW.principle is null)THEN
+		RAISE EXCEPTION 'You have to enter a principle amount';
+	ELSIF((NEW.monthly_repayment = 0) AND (NEW.repayment_period = 0))THEN
+		RAISE EXCEPTION 'You have need to enter either monthly repayment amount or repayment period';
+	ELSIF((NEW.monthly_repayment = 0) AND (NEW.repayment_period < 1))THEN
+		RAISE EXCEPTION 'The repayment period should be greater than 0';
+	ELSIF((NEW.repayment_period = 0) AND (NEW.monthly_repayment < 1))THEN
+		RAISE EXCEPTION 'The monthly repayment should be greater than 0';
+	ELSIF((NEW.monthly_repayment = 0) AND (NEW.repayment_period > 0))THEN
+		NEW.monthly_repayment := NEW.principle / NEW.repayment_period;
+	ELSIF((NEW.repayment_period = 0) AND (NEW.monthly_repayment > 0))THEN
+		NEW.repayment_period := NEW.principle / NEW.monthly_repayment;
+	END IF;
+	
 	RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
+CREATE TRIGGER ins_loans BEFORE INSERT OR UPDATE ON loans
+    FOR EACH ROW EXECUTE PROCEDURE ins_loans();
+
+
+CREATE OR REPLACE FUNCTION loan_aplication(varchar(12), varchar(12), varchar(12)) RETURNS varchar(120) AS $$
+DECLARE
+	msg 				varchar(120);
+BEGIN
+	msg := 'Loan applied';
+	
+	UPDATE loans SET approve_status = 'Completed'
+	WHERE (loan_id = CAST($1 as int)) AND (approve_status = 'Draft');
+
+	return msg;
+END;
+$$ LANGUAGE plpgsql;
+    
+
+CREATE TRIGGER upd_action BEFORE INSERT OR UPDATE ON loans
+    FOR EACH ROW EXECUTE PROCEDURE upd_action();
+    
+
+    
