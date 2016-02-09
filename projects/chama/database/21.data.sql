06979b773d614dde656ba790ef6ec4a0a5be323e
root <root@localhost.localdomain>
2016-02-09 15:25:46 +0300

    changed

--- projects/chama/database/21.data.sql
+++ projects/chama/database/21.data.sql
@@ -1,9 +1,6 @@
 
 
-INSERT INTO contribution_types VALUES(0,0,'Shares Contribution', FALSE, 'Cash raised is valued in shares');
 INSERT INTO contribution_types VALUES(1,0,'Member Contribution', TRUE, 'Cash raised for chama members');
-INSERT INTO contribution_types VALUES(2,0,'Salary Contribution', FALSE, 'Cash raised to pay chama employees');
-INSERT INTO contribution_types VALUES(3,0,'Operations Contribution', FALSE, 'Cash raised to purchase items');
 
 ALTER TABLE investments DROP COLUMN amount CASCADE;
 ALTER TABLE investments ADD COLUMN principal real;
@@ -12,11 +9,22 @@ ALTER TABLE investments ADD COLUMN monthly_returns real;
 ALTER TABLE investments ADD COLUMN total_payment real;
 ALTER TABLE investments ADD COLUMN investment_name varchar(120);
 
-alter table investment_types add interest_amount real;
+ALTER TABLE investments DROP status;
+ALTER TABLE investments ADD approve_status varchar (16) ;
 
-alter table investments drop status;
-alter table investments add approve_status varchar (16) ;
+ALTER TABLE contribution_types DROP merry_go_round CASCADE;
 
-alter table contributions add meeting_id integer references meetings;
- 
+ALTER TABLE investment_types ADD interest_amount real;
+
+ALTER TABLE contributions ADD meeting_id integer references meetings;
+ALTER TABLE contributions ADD merry_go_round_percentage real;
+ALTER TABLE contributions ADD actual_amount real;
+ALTER TABLE contributions DROP contribution_date CASCADE;
+ALTER TABLE contributions ADD merry_go_round boolean default true;
+ALTER TABLE contributions DROP confirmation CASCADE;
+ALTER TABLE contributions DROP member_payment CASCADE;
+ALTER TABLE contributions DROP share_value CASCADE;
+
+ALTER TABLE meetings ADD entity_id integer references entitys;
+
 
