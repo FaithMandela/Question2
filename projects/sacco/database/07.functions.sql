17a46800e27417978c6bbdfc0cc3792c6f1f3670
IGNATIUS <ignatius.mangeere@dewcis.com>
2016-07-20 08:29:30 +0300

    Rm Dewcis sacco

--- projects/sacco/database/07.functions.sql
+++ projects/sacco/database/07.functions.sql
@@ -568,6 +568,33 @@ FROM sacco_investments
     JOIN investment_types ON sacco_investments.investment_type_id = investment_types.investment_type_id
     LEFT JOIN bank_accounts ON sacco_investments.bank_account_id = bank_accounts.bank_account_id;
 
+    
+drop funcion change_password ( character varying, character varying,character varying);
+
+CREATE OR REPLACE FUNCTION change_password(v_entityID integer, v_old_pass varchar(32), v_pass varchar(32)) RETURNS varchar(120) AS $$
+DECLARE
+    old_password    varchar(64);
+    passchange      varchar(120);
+    entityID        integer;
+BEGIN
+    passchange := 'Password Error';
+    entityID := CAST($1 AS INT);
+    SELECT Entity_password INTO old_password FROM entitys WHERE (entity_id = entityID);
+
+    IF ($2 = '0') THEN
+        passchange := first_password();
+        UPDATE entitys SET first_password = passchange, Entity_password = md5(passchange) WHERE (entity_id = entityID);
+        passchange := 'Password Changed';
+    ELSIF (old_password = md5($2)) THEN
+        UPDATE entitys SET Entity_password = md5($3) WHERE (entity_id = entityID);
+        passchange := 'Password Changed';
+    ELSE
+        passchange := null;
+    END IF;
+
+    return passchange;
+END;
+$$ LANGUAGE plpgsql;
 
 
 
