66e4f411244bddf030970c8085f89a7ea46cec99
IGNATIUS <ignatius.mangeere@dewcis.com>
2016-07-28 06:50:55 +0300

    Modified microsrtart files

--- projects/sacco/database/07.functions.sql
+++ projects/sacco/database/07.functions.sql
@@ -65,8 +65,6 @@ LOOP
 		END IF;
 		
 
---INSERT INTO loan_monthly(loan_id, period_id, org_id, repayment)
-	--VALUES (v_id, NEW.period_id, NEW.org_id, v_loan);
 		END IF;
 		
 END LOOP;
@@ -74,6 +72,10 @@ END LOOP;
 INSERT INTO loan_repayment(loan_id, period_id, org_id, repayment_amount)
 	VALUES (v_id, NEW.period_id, NEW.org_id, NEW.deposit_amount);
 	--msg := 'Loan repaid first of Kes%' v_total_all;
+	
+	
+--INSERT INTO loan_monthly(loan_id, period_id, org_id, repayment)
+	--VALUES (v_id, NEW.period_id, NEW.org_id, v_loan);
 	
 	if (v_bal is not null) then
 	INSERT INTO investments (entity_id,investment_type_id, org_id, invest_amount, period_years)
