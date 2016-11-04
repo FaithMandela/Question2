f6815d0f0a3e87f92f5e6dfb39f6fae19647b1bd
Haron Korir <haron.korir@dewcis.com>
2016-11-04 18:22:50 +0300

    Heritage updates

--- projects/faidaplus/database/09.changes.sql
+++ projects/faidaplus/database/09.changes.sql
@@ -1,32 +1,121 @@
-CREATE OR REPLACE VIEW vw_balance AS
- SELECT a.dr, a.cr, a.order_date::date, a.org_id,a.son, a.pcc, a.entity_id, a.dr + a.bonus - a.cr AS balance, a.bonus
-   FROM ( SELECT COALESCE(vw_son_points.points, 0::real) AS dr,   0::real AS cr, vw_son_points.period AS order_date,
-            vw_son_points.org_id,vw_son_points.son, vw_son_points.pcc, vw_son_points.entity_id, COALESCE(vw_son_points.bonus, 0::real) AS bonus
-           FROM vw_son_points
-        UNION ALL
-         SELECT 0::real AS float4, vw_orders.grand_total AS order_total_amount, vw_orders.order_date, vw_orders.org_id, vw_orders.son,
-            vw_orders.pcc, vw_orders.entity_id,  0::real AS bonus
-               FROM vw_orders ) a
-  ORDER BY a.order_date DESC;
-
- CREATE OR REPLACE FUNCTION getUnclaimbalance(integer)  RETURNS real AS $$
+CREATE OR REPLACE FUNCTION generate_points(varchar(12), varchar(12), varchar(12)) RETURNS varchar(120) AS $$
 DECLARE
-    v_org_id 			integer;
-    v_function_role		text;
-    v_balance			real;
-	v_lbalance			real;
+	rec						RECORD;
+	v_period				varchar(7);
+	period					date;
+	v_start_date			date;
+	v_increment				integer;
+	v_period_id				integer;
+	v_org_id				integer;
+	v_entity_id				integer;
+	v_points				real;
+	v_points_id				integer;
+	v_root_points			integer;
+	v_amount				real;
+	msg 					varchar(120);
 BEGIN
-    v_balance = 0::real;
-    SELECT org_id,function_role INTO v_org_id, v_function_role FROM vw_entitys WHERE entity_id = $1;
 
-    IF(v_function_role = 'admin')THEN
-        SELECT COALESCE(sum(balance), 0) INTO v_balance
-        FROM vw_balance  WHERE  org_id = 0 AND order_date < '2016-01-01'::date;
-		SELECT COALESCE(sum(balance), 0) INTO v_lbalance
-        FROM vw_balance  WHERE  org_id = 0 AND pcc is not null AND son is not null  AND order_date >= '2016-01-01'::date;
-		v_balance := v_balance + v_lbalance;
+	v_period_id = $1::integer;
+	SELECT start_date, end_date, to_char(start_date, 'mmyyyy') INTO v_start_date, period, v_period
+	FROM periods WHERE period_id = v_period_id AND closed = false;
+	IF(v_period IS NULL)THEN RAISE EXCEPTION 'Period is closed'; END IF;
 
-    END IF;
-    RETURN v_balance;
+	IF(v_start_date < '2016-06-01'::date)THEN
+		v_increment := 0;
+	ELSE
+		v_increment := 2;
+	END IF;
+
+	v_root_points := 0;
+	DELETE FROM points WHERE period_id = v_period_id AND entity_id = 0;
+
+	FOR rec IN SELECT pcc, son, bookpcc, ticketperiod, totalsegs, substring(bookpcc from 1 for 6) as svcb_son
+		FROM t_sonsegs WHERE (ticketperiod = v_period) AND (totalsegs > 0)
+	LOOP
+
+		IF(1<= rec.totalsegs::integer AND rec.totalsegs::integer <=250 ) THEN
+			v_amount := 12 + v_increment;
+			v_points := rec.totalsegs * v_amount;
+		END IF;
+
+		IF(251<= rec.totalsegs::integer AND rec.totalsegs::integer <=500) THEN
+			v_amount := 16 + v_increment;
+			v_points := rec.totalsegs * v_amount;
+		END IF;
+
+		IF(rec.totalsegs::integer >=501 ) THEN
+			v_amount := 20 + v_increment;
+			v_points := rec.totalsegs * v_amount;
+		END IF;
+
+		SELECT orgs.org_id, entitys.entity_id INTO v_org_id, v_entity_id
+		FROM orgs INNER JOIN entitys ON orgs.org_id = entitys.org_id
+		WHERE (entitys.is_active = true) AND (orgs.pcc = rec.pcc) AND (entitys.son = rec.son);
+
+		IF(v_entity_id is null)THEN
+			SELECT entity_id INTO v_entity_id
+			FROM change_pccs
+			WHERE (approve_status = 'Approved') AND (pcc = rec.pcc) AND (son = rec.son);
+			SELECT org_id INTO v_org_id
+			FROM entitys
+			WHERE (entitys.is_active = true) AND (entity_id = v_entity_id);
+
+		END IF;
+
+		IF(v_entity_id is null)THEN
+			SELECT entity_id INTO v_entity_id
+			FROM entitys
+			WHERE (is_active = true) AND (svcb_son = rec.svcb_son);
+			SELECT org_id INTO v_org_id
+			FROM entitys
+			WHERE (is_active = true) AND (entity_id = v_entity_id);
+			IF(v_org_id is null)THEN v_entity_id := 0; v_org_id := 0; END IF;
+		END IF;
+
+		IF(v_entity_id is null)THEN v_entity_id := 0; v_org_id := 0; END IF;
+
+		--- Compute rooot points
+		IF(v_entity_id <> 0)THEN
+			v_root_points := v_root_points + rec.totalsegs;
+		END IF;
+
+		SELECT points_id INTO v_points_id
+		FROM points WHERE (period_id = v_period_id) AND (entity_id = v_entity_id)
+			AND (pcc = rec.pcc) AND (son = rec.son);
+
+		IF(v_points_id is null)THEN
+			SELECT points_id INTO v_points_id
+			FROM points WHERE (period_id = v_period_id) AND (entity_id = v_entity_id)
+				AND (son = rec.son);
+		END IF;
+
+		IF(v_points_id is null)THEN
+			INSERT INTO points (point_date, period_id, org_id, entity_id, pcc, son, segments, amount, points)
+			VALUES (period, v_period_id, v_org_id, v_entity_id, rec.pcc, rec.son, rec.totalsegs, v_amount, v_points);
+		ELSE
+			UPDATE points SET segments = rec.totalsegs, amount = v_amount, points = v_points
+			WHERE points_id = v_points_id;
+		END IF;
+	END LOOP;
+
+	IF(v_start_date >= '2016-06-01'::date)THEN
+		SELECT points_id INTO v_points_id
+		FROM points WHERE (period_id = v_period_id) AND (entity_id = 0) AND (pcc is null) AND (son is null);
+
+		IF(v_points_id is null )THEN
+			INSERT INTO points (point_date, period_id, org_id, entity_id, amount, points)
+			VALUES (period, v_period_id, 0, 0, 2, v_root_points * 2);
+		ELSE
+			UPDATE points SET amount = 2, points = v_root_points * 2
+			WHERE points_id = v_points_id;
+		END IF;
+	END IF;
+
+	IF(rec IS NULL)THEN
+		RAISE EXCEPTION 'There are no segments for this month';
+	ELSE
+		msg := 'Points computed';
+	END IF;
+	RETURN msg;
 END;
 $$ LANGUAGE plpgsql;
