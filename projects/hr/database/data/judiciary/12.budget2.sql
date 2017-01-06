c25a2f571f482ec20d4d867c393d5bf2241fe22a
Dennis Gichangi <dennis.gichangi@dewcis.com>
2017-01-06 15:25:11 +0300

    HR Update

---
+++ projects/hr/database/data/judiciary/12.budget2.sql
@@ -0,0 +1,131 @@
+CREATE TABLE budget2 (
+	budget2_id			serial primary key,
+	BCode				varchar(50),
+	BStation			varchar(50),
+	B2210203			varchar(50),
+	B2210301			varchar(50),
+	B2210302			varchar(50),
+	B2210303			varchar(50),
+	B2210503			varchar(50),
+	B2210801			varchar(50),
+	B2211101			varchar(50),
+	B2211102			varchar(50),
+	B2211103			varchar(50),
+	B2211201			varchar(50),
+	B2211308			varchar(50),
+	B2211310			varchar(50),
+	B2220101			varchar(50),
+	B2220205			varchar(50),
+	B2220209			varchar(50),
+	B2220210			varchar(50)
+);
+
+INSERT INTO budget2 (BCode, BStation, B2210203, B2210301, B2210302, B2210303, B2210503, B2210801, B2211101, B2211102, B2211103, B2211201, B2211308, B2211310, B2220101, B2220205, B2220209, B2220210) VALUES ('2130', 'Nyeri', '564000', '800000', '960000', '780000', '162720', '480000', '3095780', '480000', '480000', '800000', '960000', '1200000', '540000', '660000', '660000', '300000');
+INSERT INTO budget2 (BCode, BStation, B2210203, B2210301, B2210302, B2210303, B2210503, B2210801, B2211101, B2211102, B2211103, B2211201, B2211308, B2211310, B2220101, B2220205, B2220209, B2220210) VALUES ('3150', 'Malindi', '280000', '600000', '900000', '550000', '180000', '280000', '1720000', '220000', '280000', '480000', '540000', '600000', '540000', '300000', '300000', '200000');
+INSERT INTO budget2 (BCode, BStation, B2210203, B2210301, B2210302, B2210303, B2210503, B2210801, B2211101, B2211102, B2211103, B2211201, B2211308, B2211310, B2220101, B2220205, B2220209, B2220210) VALUES ('4010', 'Embu', '380000', '450000', '600000', '650000', '160000', '200000', '1000000', '600000', '240000', '1080000', '600000', '800000', '540000', '320000', '320000', '400000');
+INSERT INTO budget2 (BCode, BStation, B2210203, B2210301, B2210302, B2210303, B2210503, B2210801, B2211101, B2211102, B2211103, B2211201, B2211308, B2211310, B2220101, B2220205, B2220209, B2220210) VALUES ('4150', 'Machakos', '640000', '1200000', '1160000', '1440000', '210000', '480000', '2000000', '1440000', '600000', '1320000', '1200000', '2500000', '860000', '1200000', '600000', '600000');
+INSERT INTO budget2 (BCode, BStation, B2210203, B2210301, B2210302, B2210303, B2210503, B2210801, B2211101, B2211102, B2211103, B2211201, B2211308, B2211310, B2220101, B2220205, B2220209, B2220210) VALUES ('4310', 'Meru', '600000', '1000000', '1500000', '900000', '500000', '500000', '2000000', '600000', '500000', '750000', '2000000', '2000000', '700000', '2000000', '600000', '700000');
+INSERT INTO budget2 (BCode, BStation, B2210203, B2210301, B2210302, B2210303, B2210503, B2210801, B2211101, B2211102, B2211103, B2211201, B2211308, B2211310, B2220101, B2220205, B2220209, B2220210) VALUES ('6010', 'Kisii', '391600', '1040000', '1216000', '1028000', '116000', '202400', '1232000', '911800', '376000', '484000', '506000', '660000', '540000', '220000 ', '', '220000');
+INSERT INTO budget2 (BCode, BStation, B2210203, B2210301, B2210302, B2210303, B2210503, B2210801, B2211101, B2211102, B2211103, B2211201, B2211308, B2211310, B2220101, B2220205, B2220209, B2220210) VALUES ('6150', 'Kisumu', '440000', '506000', '990000', '1100000', '116000', '202000', '2200000', '810000', '440000', '1100000', '1100000', '1100000', '1000000', '780000 ', '', '770000');
+INSERT INTO budget2 (BCode, BStation, B2210203, B2210301, B2210302, B2210303, B2210503, B2210801, B2211101, B2211102, B2211103, B2211201, B2211308, B2211310, B2220101, B2220205, B2220209, B2220210) VALUES ('7530', 'Kericho', '320000', '380000', '480000', '400000', '180000', '160000', '1700000', '360000', '300000', '480000', '1000000', '1500000', '540000', '700000', '500000', '320000');
+INSERT INTO budget2 (BCode, BStation, B2210203, B2210301, B2210302, B2210303, B2210503, B2210801, B2211101, B2211102, B2211103, B2211201, B2211308, B2211310, B2220101, B2220205, B2220209, B2220210) VALUES ('7350', 'Nakuru', '560000', '1840000', '1200000', '1300000', '140000', '312000', '2160000', '1720000', '420000', '1920000', '1400000', '1200000', '1000000', '540000 ', '', '360000');
+INSERT INTO budget2 (BCode, BStation, B2210203, B2210301, B2210302, B2210303, B2210503, B2210801, B2211101, B2211102, B2211103, B2211201, B2211308, B2211310, B2220101, B2220205, B2220209, B2220210) VALUES ('7090', 'Kitale', '504000', '800000', '960000', '920000', '128000', '288000', '1728000', '683200', '280000', '880000', '640000', '856000', '540000', '376000 ', '', '400000');
+INSERT INTO budget2 (BCode, BStation, B2210203, B2210301, B2210302, B2210303, B2210503, B2210801, B2211101, B2211102, B2211103, B2211201, B2211308, B2211310, B2220101, B2220205, B2220209, B2220210) VALUES ('7190', 'Eldoret', '440000', '506000', '990000', '1100000', '200000', '490000', '2200000', '800000', '440000', '1100000', '1100000', '600000', '540000', '780000 ', '', '770000');
+INSERT INTO budget2 (BCode, BStation, B2210203, B2210301, B2210302, B2210303, B2210503, B2210801, B2211101, B2211102, B2211103, B2211201, B2211308, B2211310, B2220101, B2220205, B2220209, B2220210) VALUES ('9170', 'Bungoma', '504000', '1200000', '1440000', '1600000', '200000', '480000', '1600000', '800000', '480000', '800000', '800000', '1000000', '540000', '200000', '600000', '400000');
+INSERT INTO budget2 (BCode, BStation, B2210203, B2210301, B2210302, B2210303, B2210503, B2210801, B2211101, B2211102, B2211103, B2211201, B2211308, B2211310, B2220101, B2220205, B2220209, B2220210) VALUES ('5010', 'Garissa', '160000', '2790000', '2980000', '2840000', '180000', '120000', '1400000', '660000', '180000', '600000', '480000', '600000', '540000 ', '200000', '', '120000');
+INSERT INTO budget2 (BCode, BStation, B2210203, B2210301, B2210302, B2210303, B2210503, B2210801, B2211101, B2211102, B2211103, B2211201, B2211308, B2211310, B2220101, B2220205, B2220209, B2220210) VALUES ('9250', 'Busia', '280000', '740000', '780000', '700000', '180000', '280000', '1020000', '580000', '120000', '480000', '400000', '600000', '540000', '300000', '300000', '200000');
+INSERT INTO budget2 (BCode, BStation, B2210203, B2210301, B2210302, B2210303, B2210503, B2210801, B2211101, B2211102, B2211103, B2211201, B2211308, B2211310, B2220101, B2220205, B2220209, B2220210) VALUES ('9030', 'Kakamega', '556720', '510200', '646300', '510200', '160000', '324870', '1392300', '823000', '185600', '840000', '750000', '928500', '540000', '610000 ', '', '340000');
+INSERT INTO budget2 (BCode, BStation, B2210203, B2210301, B2210302, B2210303, B2210503, B2210801, B2211101, B2211102, B2211103, B2211201, B2211308, B2211310, B2220101, B2220205, B2220209, B2220210) VALUES ('3070', 'Mombasa', '1000000', '1900000', '2000000', '1600000', '280000', '1000000', '3000000', '2480000', '800000', '1600000', '3500000', '2000000', '1100000', '1000000', '600000', '600000');
+
+INSERT INTO orgs (org_id, org_name)
+SELECT cast(bcode as int), trim(bstation)
+FROM budget2;
+
+UPDATE orgs SET org_name = 'Judicially', cert_number = 'C.', pin = 'PJ', vat_number = 'A', logo = 'logo.png',
+invoice_footer = 'Make all payments to : Judicially
+Justice for all';
+
+INSERT INTO address (address_id, address_name, sys_country_id, table_name, table_id, post_office_box, postal_code, premises, street, town, phone_number, extension, mobile, fax, email, website, is_default, first_password, details) 
+SELECT cast(bcode as int), NULL, 'KE', 'orgs', cast(bcode as int), '45689', '00100', 'Registry Office', 'Supreme Court', 'Nairobi', '+254 (20) ', NULL, '+254 725 ', NULL, 'accounts@judicially.co.ke', 'www.judicially.co.ke', true, NULL, NULL
+FROM budget2;
+
+INSERT INTO entitys (entity_id, org_id, entity_type_id, user_name, entity_name, primary_email) 
+SELECT cast(bcode as int), cast(bcode as int), 0, lower(trim(bstation)), trim(bstation), lower(trim(bstation)) || '@judiciary.go.ke'
+FROM budget2;
+
+INSERT INTO departments (ln_department_id, org_id, department_id, department_name)
+SELECT 0, cast(bcode as int), cast(bcode as int), trim(bstation)
+FROM budget2;
+
+INSERT INTO budgets (fiscal_year_id, budget_type, entity_id, budget_id, org_id, department_id, budget_name)
+SELECT '2012', 2, 0, cast(bcode as int), cast(bcode as int), cast(bcode as int), trim(bstation) || ' - Main Budget'
+FROM budget2;
+
+INSERT INTO budget_lines (org_id, budget_id, period_id, account_id, item_id, amount)
+SELECT cast(bcode as int), cast(bcode as int), 1, '2210203', '2210203', cast(B2210203 as real)
+FROM budget2
+WHERE B2210203 != '';
+INSERT INTO budget_lines (org_id, budget_id, period_id, account_id, item_id, amount)
+SELECT cast(bcode as int), cast(bcode as int), 1, '2210301', '2210301', cast(B2210301 as real)
+FROM budget2
+WHERE B2210301 != '';
+INSERT INTO budget_lines (org_id, budget_id, period_id, account_id, item_id, amount)
+SELECT cast(bcode as int), cast(bcode as int), 1, '2210302', '2210302', cast(B2210302 as real)
+FROM budget2
+WHERE B2210302 != '';
+INSERT INTO budget_lines (org_id, budget_id, period_id, account_id, item_id, amount)
+SELECT cast(bcode as int), cast(bcode as int), 1, '2210303', '2210303', cast(B2210303 as real)
+FROM budget2
+WHERE B2210303 != '';
+INSERT INTO budget_lines (org_id, budget_id, period_id, account_id, item_id, amount)
+SELECT cast(bcode as int), cast(bcode as int), 1, '2210503', '2210503', cast(B2210503 as real)
+FROM budget2
+WHERE B2210503 != '';
+INSERT INTO budget_lines (org_id, budget_id, period_id, account_id, item_id, amount)
+SELECT cast(bcode as int), cast(bcode as int), 1, '2210801', '2210801', cast(B2210801 as real)
+FROM budget2
+WHERE B2210801 != '';
+INSERT INTO budget_lines (org_id, budget_id, period_id, account_id, item_id, amount)
+SELECT cast(bcode as int), cast(bcode as int), 1, '2211101', '2211101', cast(B2211101 as real)
+FROM budget2
+WHERE B2211101 != '';
+
+INSERT INTO budget_lines (org_id, budget_id, period_id, account_id, item_id, amount)
+SELECT cast(bcode as int), cast(bcode as int), 1, '2211102', '2211102', cast(B2211102 as real)
+FROM budget2
+WHERE B2211102 != '';
+INSERT INTO budget_lines (org_id, budget_id, period_id, account_id, item_id, amount)
+SELECT cast(bcode as int), cast(bcode as int), 1, '2211103', '2211103', cast(B2211103 as real)
+FROM budget2
+WHERE B2211103 != '';
+INSERT INTO budget_lines (org_id, budget_id, period_id, account_id, item_id, amount)
+SELECT cast(bcode as int), cast(bcode as int), 1, '2211201', '2211201', cast(B2211201 as real)
+FROM budget2
+WHERE B2211201 != '';
+INSERT INTO budget_lines (org_id, budget_id, period_id, account_id, item_id, amount)
+SELECT cast(bcode as int), cast(bcode as int), 1, '2211308', '2211308', cast(B2211308 as real)
+FROM budget2
+WHERE B2211308 != '';
+INSERT INTO budget_lines (org_id, budget_id, period_id, account_id, item_id, amount)
+SELECT cast(bcode as int), cast(bcode as int), 1, '2211310', '2211310', cast(B2211310 as real)
+FROM budget2
+WHERE B2211310 != '';
+INSERT INTO budget_lines (org_id, budget_id, period_id, account_id, item_id, amount)
+SELECT cast(bcode as int), cast(bcode as int), 1, '2220101', '2220101', cast(B2220101 as real)
+FROM budget2
+WHERE B2220101 != '';
+
+INSERT INTO budget_lines (org_id, budget_id, period_id, account_id, item_id, amount)
+SELECT cast(bcode as int), cast(bcode as int), 1, '2220205', '2220205', cast(B2220205 as real)
+FROM budget2
+WHERE B2220205 != '';
+INSERT INTO budget_lines (org_id, budget_id, period_id, account_id, item_id, amount)
+SELECT cast(bcode as int), cast(bcode as int), 1, '2220209', '2220209', cast(B2220209 as real)
+FROM budget2
+WHERE B2220209 != '';
+INSERT INTO budget_lines (org_id, budget_id, period_id, account_id, item_id, amount)
+SELECT cast(bcode as int), cast(bcode as int), 1, '2220210', '2220210', cast(B2220210 as real)
+FROM budget2
+WHERE B2220210 != '';
+
+
---
+++ projects/property/update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cp -u ../hr/reports/vw_accounts.jasper ./reports/
+cp -u ../hr/reports/vw_items.jasper ./reports/
+cp -u ../hr/reports/item_category.jasper ./reports/
+cp -u ../hr/reports/item_units.jasper ./reports/
+cp -u ../hr/reports/vw_ledgerb.jasper ./reports/
+cp -u ../hr/reports/vw_ledger.jasper ./reports/
+cp -u ../hr/reports/vw_gls.jasper ./reports/
+cp -u ../hr/reports/trial_balancec.jasper ./reports/
+cp -u ../hr/reports/trial_balanceb.jasper ./reports/
+cp -u ../hr/reports/trial_balance.jasper ./reports/
+cp -u ../hr/reports/vw_ie.jasper ./reports/
+cp -u ../hr/reports/balance_sheet.jasper ./reports/
+cp -u ../hr/reports/statement_a.jasper ./reports/
+cp -u ../hr/reports/statement.jasper ./reports/
+cp -u ../hr/reports/vw_helpdesk.jasper ./reports/
+cp -u ../hr/reports/vw_helpdesk_c.jasper ./reports/
+
