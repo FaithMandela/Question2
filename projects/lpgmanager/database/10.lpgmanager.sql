f75bb9552f7383c884f0964c65cf3405fd2bee72
Dennis Gichangi <dennis.gichangi@dewcis.com>
2017-07-12 17:18:51 +0300

    LPG Manager

--- projects/lpgmanager/database/10.lpgmanager.sql
+++ projects/lpgmanager/database/10.lpgmanager.sql
@@ -15,7 +15,7 @@ CREATE TABLE cylinder_batch (
 
 	quantity				integer,
 
-	approve_status			varchar(16) default 'draft' not null,
+	approve_status			varchar(16) default 'Draft' not null,
 	workflow_table_id		integer,
 	application_date		timestamp default now(),
 	action_date				timestamp,
@@ -28,8 +28,9 @@ CREATE TABLE cylinders (
 	cylinder_batch_id		integer references cylinder_batch,
 	org_id					integer references orgs,
 
-	cylinder_number			varchar(32),
-	certification_date		date,
+	cylinder_number			ineteger,
+	cylinder_code			varchar(32),
+	certification_date		timestamp default current_timestamp not null,
 	review_date				date,
 
 	checked					boolean default false not null,
@@ -40,7 +41,7 @@ CREATE TABLE check_logs (
 	check_log_id			serial primary key,
 	org_id					integer references orgs,
 	check_log_date			timestamp default current_timestamp not null,
-	cylinder_number			varchar(32),
+	cylinder_code			varchar(32),
 	verified				boolean default false not null
 );
 
@@ -61,8 +62,41 @@ CREATE VIEW vw_cylinders AS
 		vw_cylinder_batch.workflow_table_id, vw_cylinder_batch.application_date, vw_cylinder_batch.action_date,
 		orgs.org_id, orgs.org_name, 
 
-		cylinders.cylinder_id, cylinders.cylinder_number, cylinders.certification_date, cylinders.review_date, cylinders.checked, cylinders.details
+		cylinders.cylinder_id, cylinders.cylinder_number, cylinders.cylinder_code,
+		cylinders.certification_date, cylinders.review_date, cylinders.checked, cylinders.details
 	FROM cylinders INNER JOIN vw_cylinder_batch ON cylinders.cylinder_batch_id = vw_cylinder_batch.cylinder_batch_id
 	INNER JOIN orgs ON cylinders.org_id = orgs.org_id;
 
+CREATE OR REPLACE FUNCTION apply_approval(varchar(12), varchar(12), varchar(12), varchar(12)) RETURNS varchar(120) AS $$
+DECLARE
+	msg							varchar(120);
+BEGIN
 
+	IF($3 = '1')THEN
+		UPDATE cylinder_batch SET approve_status = 'Completed' 
+		WHERE (cylinder_batch_id = $1::integer) AND (approve_status = 'Draft');
+
+		msg := 'Applied for batch approval';
+	END IF;
+
+	RETURN msg;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION upd_cylinder_batch() RETURNS trigger AS $$
+BEGIN
+
+	IF(OLD.approve_status = 'Completed') AND (NEW.approve_status = 'Approved')THEN
+		INSERT INTO cylinders (cylinder_batch_id, org_id, cylinder_number)
+		SELECT NEW.cylinder_batch_id, NEW.org_id, ab
+		FROM generate_series(2, 4) ab;
+	END IF;
+
+	RETURN null;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER upd_cylinder_batch AFTER UPDATE ON cylinder_batch
+    FOR EACH ROW EXECUTE PROCEDURE upd_cylinder_batch();
+    
+    
