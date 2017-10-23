e85ac55faf5f73872fbb557bc668b490bd055d46
Dennis Gichangi <dennis.gichangi@dewcis.com>
2017-10-23 22:58:50 +0300

    HR Update

--- projects/hr/database/03.hr.sql
+++ projects/hr/database/03.hr.sql
@@ -443,6 +443,7 @@ CREATE TABLE leave_types (
 	month_limit				real default 0 not null,
 	initial_days			real default 0 not null,
 	maximum_carry			real default 0 not null,
+	maximum_days			real default 0 not null,
 	include_holiday 		boolean default false not null,
 
 	include_mon				boolean default true not null,
@@ -464,6 +465,7 @@ CREATE TABLE employee_leave_types (
 	org_id					integer references orgs,
 	leave_balance			real default 0 not null,
 	leave_starting			date default current_date not null,
+	leave_ending			date,
 	details					text
 );
 CREATE INDEX employee_leave_types_entity_id ON employee_leave_types (entity_id);
@@ -1091,7 +1093,8 @@ CREATE VIEW vw_employee_leave_types AS
 		leave_types.allowed_leave_days, leave_types.leave_days_span, leave_types.use_type,
 		leave_types.month_quota, leave_types.initial_days, leave_types.maximum_carry, leave_types.include_holiday,
 		employee_leave_types.org_id, employee_leave_types.employee_leave_type_id, employee_leave_types.leave_balance, 
-		employee_leave_types.leave_starting, employee_leave_types.details
+		employee_leave_types.leave_starting, employee_leave_types.leave_ending, employee_leave_types.details,
+		(CASE WHEN employee_leave_types.leave_ending < current_date THEN false ELSE true END) as leave_valid
 	FROM employee_leave_types INNER JOIN entitys ON employee_leave_types.entity_id = entitys.entity_id
 		INNER JOIN leave_types ON employee_leave_types.leave_type_id = leave_types.leave_type_id;
 		
